cf24d78c21c7af17ccc527257165504202c156a9
SarahEssaam <sarahessam29@hotmail.com>
2017-11-20 23:44:28 +0200

    Adding main

--- plotting.py
+++ plotting.py
@@ -20,5 +20,7 @@ def plot(signal,time_stamps=None,T=1/256,start=0,end=None):
         R_index = R_index[R_index >= start]
         R_index = R_index[R_index < end]
         plt.plot(R_index,signal[R_index], marker='*', linestyle='None', color='r')
+        plt.plot(R_index, np.max(signal[R_index])*np.ones(R_index.shape[0]), marker='*', linestyle='None', color='r')
     plt.plot(range(start,end),signal[start:end], linestyle='-', color='k')
+
 
--- trial.py
+++ trial.py
@@ -1,16 +1,19 @@
-import numpy as np
-import itertools
 import matplotlib.pyplot as plt
 from ECG_processing import ECG_Processing
 from plotting import plot
 
-e = ECG_Processing('Data2.txt')
+ecg = ECG_Processing('Data2.txt')
 plot.counter = 0
+'''
 sig,timeS = e.process_signal(20)
 plot(sig,time_stamps=timeS,end = 1000)
 R_R = e.RR_compute(time_stamps=timeS)
 plot(R_R)
-print(np.min(R_R))
 print(e.sinus_arrest_detect(R_R,timeS))
 plot(sig,time_stamps=e.sinus_arrest_detect(R_R,timeS),start=2000,end=3000)
+'''
+final_sig, time_stamps = ecg.process_signal(N=25,noise=1)
+plot(final_sig,time_stamps,end=2000)
+final_sig, time_stamps = ecg.process_signal(N=25,noise=0)
+plot(final_sig,time_stamps,end=2000)
 plt.show()
